3ae076dfc8d7ba04feffd32c4a832b96c5776ddc
gyara658 <gyara658@gmail.com>
2022-03-21 14:38:50 +0900

    fixed

--- webrick.rb
+++ webrick.rb
@@ -1,10 +1,18 @@
  # webrick.rb
 require 'webrick'
 
-server = WEBrick::HTTPServer.new({ 
+server = WEBrick::HTTPServer.new({
   :DocumentRoot => './',
   :BindAddress => '127.0.0.1',
   :Port => 8000
 })
 
 server.start
+
+server.mount_proc("/time") do |req, res|
+  # レスポンス内容を出力
+  body = "<html><body></body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end

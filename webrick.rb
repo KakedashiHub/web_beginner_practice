560a18a56a2802d0ca1290ee92cd3a1ff3c87169
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-02-29 19:40:44 +0900

    add: foods/index.html, modify: webrick.rb

--- webrick.rb
+++ webrick.rb
@@ -1,14 +1,42 @@
  # webrick.rb
-require 'webrick'
-
-server = WEBrick::HTTPServer.new({ 
-  :DocumentRoot => './',
-  :BindAddress => '127.0.0.1',
-  :Port => 8000
-})
-
-trap(:INT){
-    server.shutdown
-}
-
-server.start
+ require 'webrick'
+ require "erb"
+ 
+ server = WEBrick::HTTPServer.new({ 
+   :DocumentRoot => './',
+   :BindAddress => '127.0.0.1',
+   :Port => 8000
+ })
+ 
+ WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHandler)
+ server.config[:MimeTypes]["erb"] = "text/html"
+ 
+ foods = [
+   { id: 1, name: "りんご", category: "fruits" },
+   { id: 2, name: "バナナ", category: "fruits" },
+   { id: 3, name: "いちご", category: "fruits" },
+   { id: 4, name: "トマト", category: "vegetables" },
+   { id: 5, name: "キャベツ", category: "vegetables" },
+   { id: 6, name: "レタス", category: "vegetables" },
+ ]
+ 
+ server.mount_proc("/foods") do |req, res|
+   template = ERB.new( File.read('./foods/index.erb') )
+   
+   # ここにロジックを書く
+   @foods = foods
+   selected_category = req.query['category']
+ 
+   if selected_category != "all"
+       @foods = foods.select { |f| f[:category] == selected_category  }
+   end
+ 
+   res.body << template.result( binding )
+ end
+ 
+ trap(:INT){
+     server.shutdown
+ }
+ 
+ server.start
+ 
